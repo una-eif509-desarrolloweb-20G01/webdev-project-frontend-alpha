fd190a564b09d874942a9bec224a5f2e4f781728
osolano1991 <oscarsolanomora11@gmail.com>
2020-10-19 21:58:05 -0600

    Login

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8083/api",
+    baseURL: "http://localhost:8083/api/v1",
     headers: {
         "Content-type": "application/json"
     }
