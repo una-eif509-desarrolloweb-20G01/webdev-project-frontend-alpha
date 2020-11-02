c92114dd4599b1e3606ffade8a301fdaa7cbfce5
adanrs <rivedansanchez19@gmail.com>
2020-11-02 17:43:29 -0600

    Update http-common.js
    cambia la url del api

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8083/api/v1",
+    baseURL: "https://alpha-taskapp.herokuapp.com/api/v1/",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*",
