b5d15088f17ab001efbf512081b54eec3158affb
adanrs <rivedansanchez19@gmail.com>
2020-11-16 18:40:41 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>Project FrontEnd Alpha</h1>
+            <p>!rotinom ruoy edisni kcuts m'I ,pleH</p>
             <img src={logo} alt="logo"></img>
         </div>
         )
